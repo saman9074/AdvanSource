ed8f31388152b7a9da9d216aa6c6e6b5a4a1a1f7
saman9074 <saman9074@yahoo.com>
2018-04-23 00:10:49 +0430

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -232,19 +232,17 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {122630959,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
 This is an original bot and based on (AdvanSource).
 Copyright all right reserved and you must respect all laws.
 
-Source: https://github.com/janlou/AdvanSource
-Channel: @AdvanTeam
-Messenger: @Advanbot
-Creator: @janlou
-Site: http://StoreVps.net
-Version: [4.1]
+Channel: @ArcaneTeam
+Messenger: @saman9074
+Creator: @saman9074
+Version: [1.0]
 ]],
     --Start rate:
     Group_rate = [[]],
